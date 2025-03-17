a975ae04da911a6b7d65a0f53497a93d7889c7b3
ayynny <anzh.06.03@gmail.com>
2025-03-16 17:21:26 -0700

    trying to have image show on netlify

--- js/customelement.js
+++ js/customelement.js
@@ -2,7 +2,7 @@ let myJson = [
   {
     "title": "Rambo and I!",
     "imageWebp": "media/meandrambo.webp",
-    "image": "media/meandrambo.jpg",
+    "image": "media/meandrambo.jpg?v=1",
     "imageAlt": "A picture of the author and her Yorkie dog at a lake",
     "description": "Rambo's first time at the lake!",
     "link": "https://example.com/projectA"
